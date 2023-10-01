8db9fb12119ce9d84b821c2049eb48da0c507bc1
junsu <melon8372@gmail.com>
2023-10-01 15:38:49 +0900

    다른 컴포넌트 가져오는 작업

--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
+        <NuxtLogoVue />
         <h1>Main Page</h1>
     </div>
 </template>
 
 <script>
-export default {
+import NuxtLogoVue from '~/components/NuxtLogo.vue'
 
+export default {
+    components: {
+        NuxtLogoVue,
+    }
 }
 </script>
 
