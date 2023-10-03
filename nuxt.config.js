1b4842f1632b4500f1d714dee79b172f1ccd813c
junsu <melon8372@gmail.com>
2023-10-03 20:44:55 +0900

    메타 태그 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,17 @@
 export default {
   // Deploy Mode
   target: 'static',
-  
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-nuxt',
+    title: 'Learn Nuxt Shopping',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Nuxt Shopping Example' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
