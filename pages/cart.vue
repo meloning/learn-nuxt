1396042bd0c24a8347c422c447d14ff6dfcf55db
junsu <melon8372@gmail.com>
2023-10-03 16:44:56 +0900

    카트 리스트 컴포넌트로 리펙토링

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,17 +1,7 @@
 <template>
   <div class="container">
     <h1 class="list-title">카트 페이지</h1>
-    <div class="list-wrapper">
-      <ul>
-        <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id" class="list-item">
-          <img class="thumbnail" :src="cartItem.imageUrl" :alt="cartItem.name">
-          <div class="description">
-            <p>{{ cartItem.name }}</p>
-            <span>{{ cartItem.price }}</span>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <CartList />
     <div class="extra-panel">
       <button>구매하기</button>
     </div>
@@ -19,7 +9,9 @@
 </template>
 
 <script>
+import CartList from '~/components/CartList.vue'
 export default {
+  components: { CartList },
   // 페이지 진입하기 전 asyncData를 통해 데이터를 dispatch(다 불러와)하여 데이터를 표현하겠다라는 뜻
   // async asyncData({ store }) {
   //   await store.dispatch('fetchCartItems')
@@ -36,23 +28,6 @@ export default {
 .list-title {
   font-weight: 700;
   font-size: 1.4rem;
-}
-
-.list-wrapper {
-  margin: 0.4rem 0;
-}
-
-.list-item {
-  display: flex;
-}
-
-.thumbnail {
-  width: 100px;
-  height: 100px;
-}
-
-.description {
-  padding: 2rem 1rem;
 }
 
 .extra-panel {
