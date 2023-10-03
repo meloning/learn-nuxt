5f1e8530863917b3ba4506a90ddd3a842a0a0866
junsu <melon8372@gmail.com>
2023-10-03 15:06:26 +0900

    카트페이지 스타일 추가

--- store/index.js
+++ store/index.js
@@ -4,6 +4,10 @@ export const state = () => ({
 
 export const mutations = {
   addCartItem(state, cartItem) {
-    state.cartItems.push(cartItem)
+    const newCartItem = {
+      ...cartItem, // spread operator
+      imageUrl: `${cartItem.imageUrl}?random=${Math.random()}` // 동일한 주소에서 동일한 이미지를 가져오고 있기 때문에 캐싱될 확률이 있기에
+    }
+    state.cartItems.push(newCartItem)
   }
 }
